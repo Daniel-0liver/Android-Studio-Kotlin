4d3724e4eab82710e6a4ddecdf971d42897db83e
Daniel Oliveira <dtoliverbr@gmail.com>
2021-10-16 01:56:34 +0100

    Roll the Dice Image

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,16 +14,41 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        //Do a dice roll at the start of the app
+        rollDice()
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceRoll2 = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        val resultTextView3: TextView = findViewById(R.id.textView3)
-        resultTextView.text = diceRoll.toString()
-        resultTextView3.text = diceRoll2.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage2.setImageResource(drawableResource2)
+        diceImage2.contentDescription = diceRoll2.toString()
+
     }
 }
 
